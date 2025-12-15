0e0e18341f58c5085a10cd89ddf7cf0b79432127
bluntentertainment1-arch <bluntentertainment1@gmail.com>
2025-12-15 02:31:27 +0100

    Update MainActivity.kt

--- app/src/main/java/com/kidblunt/cleanerguru/MainActivity.kt
+++ app/src/main/java/com/kidblunt/cleanerguru/MainActivity.kt
@@ -12,14 +12,14 @@ import com.kidblunt.cleanerguru.ui.navigation.AppNavigation
 import com.kidblunt.cleanerguru.ui.theme.CleanerGuruTheme
 import android.content.Context
 import android.content.SharedPreferences
-import com.dexati.analytics.UserApi
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var sharedPreferences: SharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE)
         var userId = sharedPreferences.getString("user_id", "")
-        UserApi.logUserAnalytics("5380bb5c-c23c-4a7e-acbf-07ee7a412b56",userId!!, applicationContext,"https://api.lastapp.ai/")
+    
         
         installSplashScreen()
         
