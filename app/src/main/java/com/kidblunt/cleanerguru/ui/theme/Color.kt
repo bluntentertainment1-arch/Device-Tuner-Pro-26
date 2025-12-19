8a1bccc79e255d1ec4fb69a780ead4b4f95eea74
bluntentertainment1-arch <bluntentertainment1@gmail.com>
2025-12-19 21:00:00 +0100

    Add files via upload

---
+++ app/src/main/java/com/kidblunt/cleanerguru/ui/theme/Color.kt
@@ -0,0 +1,43 @@
+package com.kidblunt.cleanerguru.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Gen Z Vibrant Color Palette
+val NeonPink = Color(0xFFFF006E)
+val ElectricBlue = Color(0xFF00F5FF)
+val VibrantPurple = Color(0xFF8338EC)
+val NeonGreen = Color(0xFF3DDC84)
+val SunsetOrange = Color(0xFFFF8500)
+val HotMagenta = Color(0xFFFF1B8D)
+
+// Primary Colors
+val PinkPrimary = NeonPink
+val PinkPrimaryDark = Color(0xFFE6005C)
+val PinkPrimaryLight = Color(0xFFFF4D9A)
+
+// Secondary Colors
+val CloudBlue = ElectricBlue
+val CloudBlueDark = Color(0xFF00D4E6)
+val CloudBlueLight = Color(0xFF66F7FF)
+
+// Accent Colors
+val SoftSand = SunsetOrange
+val SoftSandDark = Color(0xFFE67300)
+val SoftSandLight = Color(0xFFFFB366)
+
+// Status Colors
+val SuccessGreen = NeonGreen
+val WarningOrange = SunsetOrange
+val ErrorRed = Color(0xFFFF073A)
+val InfoBlue = ElectricBlue
+
+// Gradient Colors
+val GradientStart = VibrantPurple
+val GradientMiddle = NeonPink
+val GradientEnd = ElectricBlue
+
+// Background Colors
+val DarkBackground = Color(0xFF0A0A0A)
+val LightBackground = Color(0xFFF8F8FF)
+val CardBackground = Color(0xFF1A1A2E)
+val SurfaceColor = Color(0xFF16213E)
